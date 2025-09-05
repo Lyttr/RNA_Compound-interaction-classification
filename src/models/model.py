eaeca06d050c28575eacca626983ca2c8c4532ea
Gavin Ji <71982068+Lyttr@users.noreply.github.com>
2025-09-05 11:58:12 -0700

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -164,3 +164,22 @@ class RNAFM_Drugchat(nn.Module):
         image_embeddings = self.cnn(image_data)
         combined = torch.cat((token_embeddings, graph_embeddings,image_embeddings), dim=1)
         return self.mlp(combined)
+class RNAFM_Drugchat_mean(nn.Module):
+    def __init__(self, gnn_config,mlp_hidden_dim):
+        super(RNAFM_Drugchat_mean, self).__init__()
+        data_dir = '../input/rnafm-tutorial/'
+        temp_model, alphabet = fm.pretrained.rna_fm_t12(Path(data_dir, 'RNA-FM_pretrained.pth'))
+    
+        self.fm_model=fm.BioBertModel(temp_model.args,alphabet)
+        self.gnn = GNN_graphpred(**gnn_config)
+
+        self.cnn=ImageMol("ResNet18")
+        self.mlp = MLP(input_dim=1452, hidden_dim=mlp_hidden_dim)
+
+    def forward(self, tokens, graph_data,image_data):
+        token_embeddings = self.fm_model(tokens,repr_layers=[12])['representations'][12]
+        token_embeddings = torch.mean(token_embeddings, dim=1).values 
+        graph_embeddings = self.gnn(graph_data)    
+        image_embeddings = self.cnn(image_data)
+        combined = torch.cat((token_embeddings, graph_embeddings,image_embeddings), dim=1)
+        return self.mlp(combined)
